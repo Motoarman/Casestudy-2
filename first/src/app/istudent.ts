64cf63642d82503b40c8e9af76435182a9fc5752
anushkadhawan99 <anushkadhawan03@gmail.com>
2022-04-20 16:35:54 +0530

    changes

--- first/src/app/istudent.ts
+++
@@ -1,9 +0,0 @@
-export interface Istudent {
-    contact_id: number;
-    firstname: string;
-    lastname: string;
-    email_id:number;
-    p_comment:string;
-}
-
-
--- first/src/app/user-details.ts
+++
@@ -1,6 +0,0 @@
-export interface UserDetails {
-    firstname:string;
-    lastname :string;
-    emailId :string;
-    pComment :string;
-}
