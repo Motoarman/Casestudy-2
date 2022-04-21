0fdacc97f5168789443f392f9e86dc1e29b683ac
arman <Arman15411@mastek.com>
2022-04-22 00:40:10 +0530

    last commit

---
+++ first/src/app/doctor-details.ts
@@ -0,0 +1,8 @@
+export interface DoctorDetails {
+    FirstName : string;
+    LastName : string;
+    PhoneNo : String;
+    EmailId : String;
+    SpecializationId : Number;
+    Photo : String;
+}
